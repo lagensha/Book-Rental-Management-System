e4118e9f3eecaed95159862b148cb7ef92730dc8
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-05-28 22:47:26 +0530

    Refactor DBConnection to implement singleton pattern and update database connection details

--- src/main/java/edu/icet/ecom/db/DBConnection.java
+++ src/main/java/edu/icet/ecom/db/DBConnection.java
@@ -2,12 +2,20 @@ package edu.icet.ecom.db;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DBConnection {
 private static DBConnection instance;
 private Connection connection;
 
-private DBConnection(){
-    connection= DriverManager.getConnection("jdbc:mysql://localhost:3306/ecom_db","root","password");
+private DBConnection DBConnection() throws SQLException {
+    connection= DriverManager.getConnection("jdbc:mysql://localhost:3306/Book_Rentel_System","root","12345");
+    if(instance == null){
+        instance=new DBConnection();
+    }
+    return instance;
+}
+public  Connection getConnection() {
+    return connection;
 }
 }
