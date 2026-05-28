95fc052d86c6abffd80b26d521a6ad53661baaa4
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-05-28 23:42:39 +0530

    Add registration form components to SignInPage.fxml and update SignInPageController

--- src/main/java/edu/icet/ecom/controller/SignInPageController.java
+++ src/main/java/edu/icet/ecom/controller/SignInPageController.java
@@ -1,4 +1,30 @@
 package edu.icet.ecom.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class SignInPageController {
+
+    @FXML
+    private Button btnRegister;
+
+    @FXML
+    private TextField txtEmail;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private TextField txtPassword;
+
+    @FXML
+    private TextField txtPhoneNumber;
+
+    @FXML
+    void btnRegisterOnAction(ActionEvent event) {
+
+    }
+
 }
