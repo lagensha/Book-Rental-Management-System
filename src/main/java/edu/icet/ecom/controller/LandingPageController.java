6356bf0fd41ac99d61faf20dd7eb7ae8fb3ac46d
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-05-29 00:22:40 +0530

    Enhance LandingPage with UI components and button functionality for navigation

--- src/main/java/edu/icet/ecom/controller/LandingPageController.java
+++ src/main/java/edu/icet/ecom/controller/LandingPageController.java
@@ -1,4 +1,35 @@
 package edu.icet.ecom.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class LandingPageController {
+
+    @FXML
+    private Button btnLogin;
+
+    @FXML
+    private Button btnSignIn;
+
+    Stage stage = new Stage();
+    @FXML
+    void btnLoginOnAction(ActionEvent event) {
+        try {
+            stage.setScene(FXMLLoader.load(getClass().getResource("/view/LoginPage.fxml")));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        stage.show();
+    }
+
+    @FXML
+    void btnSignInOnAction(ActionEvent event) {
+
+    }
+
 }
