d7b61ead802c476f482f9b33a4ed97a5a66af514
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-05-28 23:20:39 +0530

    Add UserPage.fxml and UserController with basic user management UI components

--- src/main/java/edu/icet/ecom/controller/UserController.java
+++ src/main/java/edu/icet/ecom/controller/UserController.java
@@ -1,4 +1,71 @@
 package edu.icet.ecom.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class UserController {
+
+    @FXML
+    private Button btnCreate;
+
+    @FXML
+    private Button btnDelete;
+
+    @FXML
+    private Button btnReset;
+
+    @FXML
+    private Button btnUpdate;
+
+    @FXML
+    private TableColumn<?, ?> colEmail;
+
+    @FXML
+    private TableColumn<?, ?> colId;
+
+    @FXML
+    private TableColumn<?, ?> colName;
+
+    @FXML
+    private TableColumn<?, ?> colPhoneNumber;
+
+    @FXML
+    private TableView<?> tblUserTable;
+
+    @FXML
+    private TextField txtEmail;
+
+    @FXML
+    private TextField txtId;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private TextField txtPhoneNumber;
+
+    @FXML
+    void btnCreateOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDeleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnResetOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUpdateOnAction(ActionEvent event) {
+
+    }
+
 }
