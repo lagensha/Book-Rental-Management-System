c68373253032f22f6c69df41f015048f95f01a31
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-05-28 22:30:12 +0530

    Add Lombok annotations to UserDTO for improved code simplicity

--- src/main/java/edu/icet/ecom/dto/UserDTO.java
+++ src/main/java/edu/icet/ecom/dto/UserDTO.java
@@ -1,6 +1,12 @@
 package edu.icet.ecom.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserDTO {
     private String id;
     private String username;
