4a9ec17ff95db901b1186b953d2446c68f916a15
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-05-28 22:24:14 +0530

    Add User-related classes and update LoginPageController

--- src/main/java/edu/icet/ecom/controller/LoginPageController.java
+++ src/main/java/edu/icet/ecom/controller/LoginPageController.java
@@ -2,9 +2,12 @@ package edu.icet.ecom.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class LoginPageController {
 
@@ -20,9 +23,10 @@ public class LoginPageController {
     @FXML
     private TextField txtUserName;
 
+    Stage stage= new Stage();
     @FXML
     void btnLoginOnAction(ActionEvent event) {
-
+//stage.setScene(new Scene(FXMLLoader.load(getClass().getResource())));
     }
 
 }
