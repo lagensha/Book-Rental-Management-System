b0447460c1d97fe98b4d05087846609f0ccd629a
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-05-28 16:27:05 +0530

    Enhance login page UI and controller with additional fields for username, password, email, and role selection

--- src/main/java/edu/icet/ecom/controller/LoginPageController.java
+++ src/main/java/edu/icet/ecom/controller/LoginPageController.java
@@ -1,5 +1,28 @@
 package edu.icet.ecom.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
 public class LoginPageController {
+
+    @FXML
+    private Button btnLogin;
+
+    @FXML
+    private ComboBox<?> cmbRole;
+
+    @FXML
+    private TextField txtPassword;
+
+    @FXML
+    private TextField txtUserName;
+
+    @FXML
+    void btnLoginOnAction(ActionEvent event) {
+
+    }
 
 }
--- src/main/java/edu/icet/ecom/entity/LoginPageEntity.java
+++ src/main/java/edu/icet/ecom/entity/LoginPageEntity.java
@@ -1,5 +1,12 @@
 package edu.icet.ecom.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
 public class LoginPageEntity {
     private String username;
     private String password;
