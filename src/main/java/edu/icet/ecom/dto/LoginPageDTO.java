2cadf9bfaf9c2885e41b818c3049d2092972eb08
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-05-28 14:37:34 +0530

    Add Lombok annotations to LoginPageDTO and include Lombok dependency in pom.xml

--- src/main/java/edu/icet/ecom/dto/LoginPageDTO.java
+++ src/main/java/edu/icet/ecom/dto/LoginPageDTO.java
@@ -1,5 +1,14 @@
 package edu.icet.ecom.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class LoginPageDTO {
     private String username;
     private String password;
