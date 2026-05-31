d3298b827a9e14da9885e027fd4298ae0c35a82b
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-05-31 10:32:47 +0530

    Enhance DBConnection to establish a connection to the MySQL database

--- src/main/java/edu/icet/ecom/db/DBConnection.java
+++ src/main/java/edu/icet/ecom/db/DBConnection.java
@@ -1,12 +1,16 @@
 package edu.icet.ecom.db;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
 private static DBConnection instance;
 private Connection connection;
 
+private DBConnection() throws SQLException {
+    connection= DriverManager.getConnection("jdbc:mysql://localhost:3306/book_rentel_system","root","12345");
+}
     public  static DBConnection getInstance() throws SQLException {
         if(instance == null){
             instance=new DBConnection();
