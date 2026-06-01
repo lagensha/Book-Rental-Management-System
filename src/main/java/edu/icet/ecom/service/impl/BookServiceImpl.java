ffc2b8e1b5a05479329489acde0611959b53489f
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-06-01 22:11:45 +0530

    Implement book management methods in BookServiceImpl and update BookPage.fxml for quantity column

--- src/main/java/edu/icet/ecom/service/impl/BookServiceImpl.java
+++ src/main/java/edu/icet/ecom/service/impl/BookServiceImpl.java
@@ -1,4 +1,51 @@
 package edu.icet.ecom.service.impl;
 
-public class BookServiceImpl {
+import edu.icet.ecom.dto.BookDTO;
+import edu.icet.ecom.repository.impl.BookRepositoryImpl;
+import edu.icet.ecom.service.BookService;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class BookServiceImpl implements BookService {
+    BookRepositoryImpl bookRepository = new BookRepositoryImpl();
+    @Override
+    public void addBook(BookDTO bookDTO) {
+
+    }
+
+    @Override
+    public void updateBook(String title, String author, String category, Integer quantity, String id) {
+
+    }
+
+    @Override
+    public void deleteBook(String id) {
+
+    }
+
+    @Override
+    public ObservableList<BookDTO> getAllBook() {
+        ObservableList<BookDTO> bookDTOS = FXCollections.observableArrayList();
+        BookRepositoryImpl bookRepository1 = new BookRepositoryImpl();
+        try {
+            ResultSet resultSet = bookRepository1.getAllBook();
+            while (resultSet.next()){
+                bookDTOS.add(new BookDTO(
+                        resultSet.getString("Id"),
+                        resultSet.getString("Title"),
+                        resultSet.getString("Author"),
+                        resultSet.getString("Category"),
+                        resultSet.getInt("Quantity")
+                ));
+            }
+            return bookDTOS;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
