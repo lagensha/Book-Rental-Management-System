645ab1b2707b310175e2697e8c88541e21ee37b6
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-06-01 08:28:20 +0530

    Add getAllAdmins method to UserServiceImpl for retrieving admin users

--- src/main/java/edu/icet/ecom/service/UserService.java
+++ src/main/java/edu/icet/ecom/service/UserService.java
@@ -1,12 +1,11 @@
 package edu.icet.ecom.service;
 
 import edu.icet.ecom.dto.UserDTO;
-
-import java.sql.ResultSet;
+import javafx.collections.ObservableList;
 
 public interface UserService {
     void addUser(UserDTO userDTO);
     void updateUser(String username,String phoneNumber,String email,String id);
     void deleteUser(String id);
-
+    ObservableList<UserDTO> getAllAdmins();
 }
--- src/main/java/edu/icet/ecom/service/impl/UserServiceImpl.java
+++ src/main/java/edu/icet/ecom/service/impl/UserServiceImpl.java
@@ -1,11 +1,18 @@
 package edu.icet.ecom.service.impl;
 
 import edu.icet.ecom.dto.UserDTO;
+import edu.icet.ecom.repository.impl.UserRepositoryImpl;
 import edu.icet.ecom.service.UserService;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class UserServiceImpl implements UserService{
+
+    UserRepositoryImpl userRepository = new UserRepositoryImpl();
+
     @Override
     public void addUser(UserDTO userDTO) {
 
@@ -21,4 +28,24 @@ public class UserServiceImpl implements UserService{
 
     }
 
-   }
+    @Override
+    public ObservableList<UserDTO> getAllAdmins() {
+        ObservableList<UserDTO>userDTOObservableList= FXCollections.observableArrayList();
+        UserRepositoryImpl userRepository1 = new UserRepositoryImpl();
+        try {
+            ResultSet resultSet=userRepository1.viewUsers();
+            while (resultSet.next()){
+                userDTOObservableList.add(new UserDTO(
+                        resultSet.getString("Id"),
+                        resultSet.getString("Username"),
+                        resultSet.getString("PhoneNumber"),
+                        resultSet.getString("Email")
+                ));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return userDTOObservableList;
+    }
+
+}
