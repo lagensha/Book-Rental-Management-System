64dc85d6fd1ca1b4992a818c3dbc85ae2ebbacd8
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-06-01 08:04:48 +0530

    Remove unused import of UserServiceImpl in UserController

--- src/main/java/edu/icet/ecom/controller/UserController.java
+++ src/main/java/edu/icet/ecom/controller/UserController.java
@@ -1,6 +1,6 @@
 package edu.icet.ecom.controller;
 
-import edu.icet.ecom.service.impl.UserServiceImpl;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
