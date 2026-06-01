f108e4dddad7bc1320888772475ca83007813942
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-06-01 12:34:02 +0530

    Add methods for book management in BookRepository interface

--- src/main/java/edu/icet/ecom/repository/BookRepository.java
+++ src/main/java/edu/icet/ecom/repository/BookRepository.java
@@ -1,4 +1,11 @@
 package edu.icet.ecom.repository;
 
+import edu.icet.ecom.dto.BookDTO;
+import javafx.collections.ObservableList;
+
 public interface BookRepository {
+    void addBook(BookDTO bookDTO);
+    void updateBook(String title,String author,String category,Integer quantity,String id);
+    void deleteBook(String id);
+    ObservableList<BookDTO> getAllBook();
 }
