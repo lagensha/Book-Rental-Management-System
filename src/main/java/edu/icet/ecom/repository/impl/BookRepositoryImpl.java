ff22fa82c2056038302ac0837a6331e9ae99e5b5
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-06-01 12:34:51 +0530

    Implement BookRepository methods for adding, updating, deleting, and retrieving books

--- src/main/java/edu/icet/ecom/repository/impl/BookRepositoryImpl.java
+++ src/main/java/edu/icet/ecom/repository/impl/BookRepositoryImpl.java
@@ -1,4 +1,28 @@
 package edu.icet.ecom.repository.impl;
 
-public class BookRepositoryImpl {
+import edu.icet.ecom.dto.BookDTO;
+import edu.icet.ecom.repository.BookRepository;
+import javafx.collections.ObservableList;
+
+
+public class BookRepositoryImpl implements BookRepository {
+    @Override
+    public void addBook(BookDTO bookDTO) {
+
+    }
+
+    @Override
+    public void updateBook(String title, String author, String category, Integer quantity, String id) {
+
+    }
+
+    @Override
+    public void deleteBook(String id) {
+
+    }
+
+    @Override
+    public ObservableList<BookDTO> getAllBook() {
+        return null;
+    }
 }
