7fff0b2a6dec8e475ca2fc559b4ed29ba8931814
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-06-01 12:29:42 +0530

    Add Lombok annotations to BookDTO for boilerplate reduction

--- src/main/java/edu/icet/ecom/dto/BookDTO.java
+++ src/main/java/edu/icet/ecom/dto/BookDTO.java
@@ -1,6 +1,14 @@
 package edu.icet.ecom.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class BookDTO {
     private String title;
     private String author;
