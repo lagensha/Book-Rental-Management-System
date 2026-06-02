244c2f80f54f32ddf52f629d995503e03222190f
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-06-02 19:11:53 +0530

    Implement PlaceOrderBookController and update PlaceOrderBookRepository interface

--- src/main/java/edu/icet/ecom/controller/PlaceOrderBookController.java
+++ src/main/java/edu/icet/ecom/controller/PlaceOrderBookController.java
@@ -1,4 +1,78 @@
 package edu.icet.ecom.controller;
 
+import edu.icet.ecom.db.DBConnection;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class PlaceOrderBookController {
+
+    @FXML
+    private Button btnAddToCart;
+
+    @FXML
+    private Button btnPlaceOrder;
+
+    @FXML
+    private ComboBox<?> cmbBookId;
+
+    @FXML
+    private ComboBox<String> cmbCustomerId;
+
+    @FXML
+    private TableColumn<?, ?> colBookId;
+
+    @FXML
+    private TableColumn<?, ?> colCustomerId;
+
+    @FXML
+    private TableColumn<?, ?> colQuantity;
+
+    @FXML
+    private TableColumn<?, ?> colTotal;
+
+    @FXML
+    private Label lblCount;
+
+    @FXML
+    private AnchorPane lblNetTotal;
+
+    @FXML
+    private TableView<?> tblBooksOrder;
+
+    @FXML
+    private TextField txtQuantity;
+
+    @FXML
+    void btnAddToCartOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnPlaceOrderOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void cmbCustomerIdOnAction(ActionEvent event) {
+    }
+
+    @FXML
+    void cmdBookIdOnAction(ActionEvent event) {
+
+    }
+
 }
--- src/main/java/edu/icet/ecom/repository/PlaceOrderBookRepository.java
+++ src/main/java/edu/icet/ecom/repository/PlaceOrderBookRepository.java
@@ -1,4 +1,6 @@
 package edu.icet.ecom.repository;
 
 public interface PlaceOrderBookRepository {
+    void loadCustomer(String id);
+    void loadBook(String id);
 }
