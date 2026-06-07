e0704badaac968a59252f3562a506b77d5b24dcd
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-06-07 19:22:58 +0530

    Add book rental DTOs to list in BookRentalServiceImpl while fetching all books

--- src/main/java/edu/icet/ecom/service/impl/BookRentalServiceImpl.java
+++ src/main/java/edu/icet/ecom/service/impl/BookRentalServiceImpl.java
@@ -24,6 +24,13 @@ public class BookRentalServiceImpl implements BookRentalService {
         BookRepositoryImpl bookRepository1 = new BookRepositoryImpl();
         try {
             ResultSet resultSet = bookRepository1.getAllBook();
+            while (resultSet.next()) {
+            bookRentDTOS.add(new BookRentDTO(
+                 resultSet.getInt("Book_Id"),
+                 resultSet.getInt("Customer_Id"),
+                    resultSet.getInt("Quantity")
+            ));
+            }
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
