f4a73134bde1d789cba7e01a06162cbc5971d285
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-06-07 23:15:05 +0530

    Update BookRentalService interface to return ObservableList of BookRentDTO for user rentals

--- src/main/java/edu/icet/ecom/service/BookRentalService.java
+++ src/main/java/edu/icet/ecom/service/BookRentalService.java
@@ -1,11 +1,13 @@
 package edu.icet.ecom.service;
 
 import edu.icet.ecom.dto.BookRentDTO;
+import edu.icet.ecom.dto.CustomerDTO;
+import javafx.collections.ObservableList;
 
 import java.sql.ResultSet;
 
 public interface BookRentalService {
     void AddBookRent(BookRentDTO bookRentDTO);
-    ResultSet getAllBookRent();
+    ObservableList<BookRentDTO> getAllUser();
     void RentBook(BookRentDTO bookRentDTO);
 }
