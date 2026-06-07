2277205bfc4e0be08752d007c3eadd72b68c268b
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-06-07 12:33:43 +0530

    Update BookRentalService and BookRentalServiceImpl to use BookRentDTO in rental methods

--- src/main/java/edu/icet/ecom/service/BookRentalService.java
+++ src/main/java/edu/icet/ecom/service/BookRentalService.java
@@ -1,9 +1,11 @@
 package edu.icet.ecom.service;
 
+import edu.icet.ecom.dto.BookRentDTO;
+
 import java.sql.ResultSet;
 
 public interface BookRentalService {
-    void AddBookRent();
-    void getAllBookRent();
-    ResultSet RentBook();
+    void AddBookRent(BookRentDTO bookRentDTO);
+    ResultSet getAllBookRent();
+    void RentBook(BookRentDTO bookRentDTO);
 }
--- src/main/java/edu/icet/ecom/service/impl/BookRentalServiceImpl.java
+++ src/main/java/edu/icet/ecom/service/impl/BookRentalServiceImpl.java
@@ -1,22 +1,24 @@
 package edu.icet.ecom.service.impl;
 
+import edu.icet.ecom.dto.BookRentDTO;
 import edu.icet.ecom.service.BookRentalService;
 
 import java.sql.ResultSet;
 
 public class BookRentalServiceImpl implements BookRentalService {
+
     @Override
-    public void AddBookRent() {
+    public void AddBookRent(BookRentDTO bookRentDTO) {
 
     }
 
     @Override
-    public void getAllBookRent() {
-
-    }
-
-    @Override
-    public ResultSet RentBook() {
+    public ResultSet getAllBookRent() {
         return null;
+    }
+
+    @Override
+    public void RentBook(BookRentDTO bookRentDTO) {
+
     }
 }
