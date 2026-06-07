f433ff387223df70902964f12944ff277ebc89ab
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-06-07 12:37:52 +0530

    Update getAllBookRent method in BookRentRepositoryImpl to prepare SQL statement

--- src/main/java/edu/icet/ecom/repository/impl/BookRentRepositoryImpl.java
+++ src/main/java/edu/icet/ecom/repository/impl/BookRentRepositoryImpl.java
@@ -21,7 +21,7 @@ public class BookRentRepositoryImpl implements BookRentRepository {
     @Override
     public ResultSet getAllBookRent() throws SQLException {
         Connection connection= DBConnection.getInstance().getConnection();
-
+        PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM  rentbooks");
     }
 
     @Override
