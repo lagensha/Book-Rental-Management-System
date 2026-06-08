48ba9f5f41e234efe59c2a6a77d2e8892a4f1fdb
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-06-08 09:02:46 +0530

    Add SQLException handling to AddBookRent method in BookRentRepository

--- src/main/java/edu/icet/ecom/repository/BookRentRepository.java
+++ src/main/java/edu/icet/ecom/repository/BookRentRepository.java
@@ -6,7 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public interface BookRentRepository {
-   void AddBookRent(BookRentDTO bookRentDTO);
+   void AddBookRent(BookRentDTO bookRentDTO) throws SQLException;
     ResultSet getAllBookRent() throws SQLException;
    void RentBook(BookRentDTO bookRentDTO);
 }
