c0c69e719ca1c432deadf42d25467bb8ea31d9dc
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-06-08 09:00:05 +0530

    Update BookRentalService interface to return ObservableList of BookRentDTO for user rentals

--- src/main/java/edu/icet/ecom/service/impl/BookRentalServiceImpl.java
+++ src/main/java/edu/icet/ecom/service/impl/BookRentalServiceImpl.java
@@ -1,7 +1,8 @@
 package edu.icet.ecom.service.impl;
 
 import edu.icet.ecom.dto.BookRentDTO;
-import edu.icet.ecom.repository.impl.BookRepositoryImpl;
+import edu.icet.ecom.dto.CustomerDTO;
+import edu.icet.ecom.repository.impl.BookRentRepositoryImpl;
 import edu.icet.ecom.service.BookRentalService;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -11,16 +12,33 @@ import java.sql.SQLException;
 
 public class BookRentalServiceImpl implements BookRentalService {
 
-    BookRepositoryImpl bookRepository = new BookRepositoryImpl();
-
+  BookRentRepositoryImpl bookRentRepository = new BookRentRepositoryImpl();
     @Override
     public void AddBookRent(BookRentDTO bookRentDTO) {
+        try {
+            bookRentRepository.AddBookRent(bookRentDTO);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
     @Override
-    public ResultSet getAllBookRent() {
-        return null;
+    public ObservableList<BookRentDTO> getAllUser(){
+       ObservableList<BookRentDTO> rentDTOObservableList = FXCollections.observableArrayList();
+        try {
+            ResultSet resultSet = bookRentRepository.getAllBookRent();
+            while (resultSet.next()){
+                rentDTOObservableList.add(new BookRentDTO(
+                        resultSet.getString("bookId"),
+                        resultSet.getString("customerId"),
+                        resultSet.getInt("quantity")
+                ));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return rentDTOObservableList;
     }
 
     @Override
