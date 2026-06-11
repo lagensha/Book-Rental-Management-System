d10128bd743b5b9043f3af773a5ce2b3455a6955
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-06-11 09:25:11 +0530

    Remove unused import statements from BookRentalServiceImpl

--- src/main/java/edu/icet/ecom/service/impl/BookRentalServiceImpl.java
+++ src/main/java/edu/icet/ecom/service/impl/BookRentalServiceImpl.java
@@ -1,13 +1,8 @@
 package edu.icet.ecom.service.impl;
 
 import edu.icet.ecom.dto.BookRentDTO;
-import edu.icet.ecom.repository.impl.BookRentRepositoryImpl;
 import edu.icet.ecom.service.BookRentalService;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
 public class BookRentalServiceImpl implements BookRentalService {
 
