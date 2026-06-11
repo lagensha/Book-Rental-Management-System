8c72135cc66386fbde7337af778c00adb6c46065
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-06-11 09:58:12 +0530

    Implement rentBook method in BookRentRepositoryImpl to handle book rental transactions

--- src/main/java/edu/icet/ecom/repository/impl/BookRentRepositoryImpl.java
+++ src/main/java/edu/icet/ecom/repository/impl/BookRentRepositoryImpl.java
@@ -1,18 +1,27 @@
 package edu.icet.ecom.repository.impl;
 
 
+import edu.icet.ecom.db.DBConnection;
 import edu.icet.ecom.dto.BookRentDTO;
 import edu.icet.ecom.repository.BookRentRepository;
 
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 
 public class BookRentRepositoryImpl implements BookRentRepository {
 
     @Override
-    public boolean rentBook(BookRentDTO bookRentDTO)  {
-        return false;
-    }
+    public boolean rentBook(BookRentDTO bookRentDTO) throws SQLException {
+        Connection connection= DBConnection.getInstance().getConnection();
+       PreparedStatement preparedStatement= connection.prepareStatement("INSERT INTO  rentbooks (Book_Id,Customer_Id,Rent_Date,Return_Date) VALUES (?,?,?,?)");
+        preparedStatement.setString(1, bookRentDTO.getBookId());
+        preparedStatement.setString(2, bookRentDTO.getCustomerId());
+        preparedStatement.setInt(3,bookRentDTO.getQuantity());
 
+        return  preparedStatement.executeUpdate() > 0;
+
+    }
 }
