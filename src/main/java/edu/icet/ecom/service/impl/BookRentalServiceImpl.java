27f5202f7fc33f0d0282bedd50206093647bae47
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-06-11 09:47:31 +0530

    Refactor BookRentalServiceImpl to use RepositoryFactory for repository instantiation and update rentBook method implementation

--- src/main/java/edu/icet/ecom/service/impl/BookRentalServiceImpl.java
+++ src/main/java/edu/icet/ecom/service/impl/BookRentalServiceImpl.java
@@ -1,15 +1,16 @@
 package edu.icet.ecom.service.impl;
 
 import edu.icet.ecom.dto.BookRentDTO;
+import edu.icet.ecom.repository.BookRentRepository;
+import edu.icet.ecom.repository.RepositoryFactory;
 import edu.icet.ecom.service.BookRentalService;
 
 
 public class BookRentalServiceImpl implements BookRentalService {
-
+     BookRentRepository bookRentRepository = RepositoryFactory.getInstance().getRepository(RepositoryFactory.RepositoryType.BOOK_RENTAL);
 
     @Override
     public boolean rentBook(BookRentDTO bookRentDTO) {
-        System.out.println(bookRentDTO);
-        return false;
+        return bookRentRepository.rentBook(bookRentDTO);
     }
 }
