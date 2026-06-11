72e3d86328b6896f3ac34ec7aa767505a68c53ed
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-06-11 11:26:54 +0530

    Remove unused imports and clean up BookRentalService interface

--- src/main/java/edu/icet/ecom/service/BookRentalService.java
+++ src/main/java/edu/icet/ecom/service/BookRentalService.java
@@ -1,15 +1,10 @@
 package edu.icet.ecom.service;
 
 import edu.icet.ecom.dto.BookRentDTO;
-import edu.icet.ecom.dto.CustomerDTO;
-import javafx.collections.ObservableList;
 
-import java.awt.print.Book;
-import java.sql.ResultSet;
-import java.util.List;
 
 public interface BookRentalService {
     boolean rentBook(BookRentDTO bookRentDTO);
-    boolean updateStock(List<BookRentDTO> bookRentDTO);
+
 
 }
