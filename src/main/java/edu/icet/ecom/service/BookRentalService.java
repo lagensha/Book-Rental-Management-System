656cc35cc4226b2e5abd5fee321f93654a44db97
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-06-11 11:06:52 +0530

    Add updateStock method to BookRentalService for managing book rental stock

--- src/main/java/edu/icet/ecom/service/BookRentalService.java
+++ src/main/java/edu/icet/ecom/service/BookRentalService.java
@@ -4,8 +4,12 @@ import edu.icet.ecom.dto.BookRentDTO;
 import edu.icet.ecom.dto.CustomerDTO;
 import javafx.collections.ObservableList;
 
+import java.awt.print.Book;
 import java.sql.ResultSet;
+import java.util.List;
 
 public interface BookRentalService {
     boolean rentBook(BookRentDTO bookRentDTO);
+    boolean updateStock(List<BookRentDTO> bookRentDTO);
+
 }
