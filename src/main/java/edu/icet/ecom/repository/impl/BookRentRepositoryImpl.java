00000f439c287267b8a226b2f36af9b6f49ff9d0
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-06-11 09:48:21 +0530

    Refactor BookRentRepository interface to simplify rentBook method and remove unused methods

--- src/main/java/edu/icet/ecom/repository/impl/BookRentRepositoryImpl.java
+++ src/main/java/edu/icet/ecom/repository/impl/BookRentRepositoryImpl.java
@@ -1,39 +1,18 @@
 package edu.icet.ecom.repository.impl;
 
-import edu.icet.ecom.db.DBConnection;
+
 import edu.icet.ecom.dto.BookRentDTO;
 import edu.icet.ecom.repository.BookRentRepository;
 
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
+
 
 public class BookRentRepositoryImpl implements BookRentRepository {
 
-
     @Override
-    public void AddBookRent(BookRentDTO bookRentDTO) throws SQLException {
-        Connection connection = DBConnection.getInstance().getConnection();
-        try (PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO rentbooks(bookId,customerId,quantity) VALUES (?,?,?)")) {
-            preparedStatement.setObject(1, bookRentDTO.getBookId());
-            preparedStatement.setObject(2, bookRentDTO.getCustomerId());
-            preparedStatement.setObject(3, bookRentDTO.getQuantity());
-            preparedStatement.executeUpdate();
-        }
-
+    public boolean rentBook(BookRentDTO bookRentDTO)  {
+        return false;
     }
 
-    @Override
-    public ResultSet getAllBookRent() throws SQLException {
-        Connection connection= DBConnection.getInstance().getConnection();
-        PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM  rentbooks");
-        return preparedStatement.executeQuery();
-    }
-
-    @Override
-    public void RentBook(BookRentDTO bookRentDTO) {
-
-    }
 }
