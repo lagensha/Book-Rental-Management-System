9f93e249a61be629bc3f8f8a02ecd2f0ab8915f8
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-06-11 09:47:48 +0530

    Refactor BookRentRepository interface to simplify rentBook method and remove unused methods

--- src/main/java/edu/icet/ecom/repository/BookRentRepository.java
+++ src/main/java/edu/icet/ecom/repository/BookRentRepository.java
@@ -2,11 +2,8 @@ package edu.icet.ecom.repository;
 
 import edu.icet.ecom.dto.BookRentDTO;
 
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public interface BookRentRepository {
-   void AddBookRent(BookRentDTO bookRentDTO) throws SQLException;
-    ResultSet getAllBookRent() throws SQLException;
-   void RentBook(BookRentDTO bookRentDTO);
+boolean rentBook(BookRentDTO bookRentDTO) throws SQLException;
 }
