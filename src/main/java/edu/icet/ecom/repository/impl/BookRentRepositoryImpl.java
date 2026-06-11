e166cfb03186a72f9b2445a441d5ccc47015bf16
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-06-11 11:27:41 +0530

    Update rentBook method in BookRentRepositoryImpl to include Quantity in rent record

--- src/main/java/edu/icet/ecom/repository/impl/BookRentRepositoryImpl.java
+++ src/main/java/edu/icet/ecom/repository/impl/BookRentRepositoryImpl.java
@@ -3,7 +3,6 @@ package edu.icet.ecom.repository.impl;
 
 import edu.icet.ecom.db.DBConnection;
 import edu.icet.ecom.dto.BookRentDTO;
-import edu.icet.ecom.repository.BookRentDetailsRepository;
 import edu.icet.ecom.repository.BookRentRepository;
 
 
@@ -17,10 +16,11 @@ BookRentDetailsRepositoryImpl bookRentDetailsRepository=new BookRentDetailsRepos
     @Override
     public boolean rentBook(BookRentDTO bookRentDTO) throws SQLException {
         Connection connection= DBConnection.getInstance().getConnection();
-       PreparedStatement preparedStatement= connection.prepareStatement("INSERT INTO  rentbooks (Book_Id,Customer_Id,Rent_Date,Return_Date) VALUES (?,?,?,?)");
+       PreparedStatement preparedStatement= connection.prepareStatement("INSERT INTO  rentbooks (Book_Id,Customer_Id,Quantity) VALUES (?,?,?)");
         preparedStatement.setString(1, bookRentDTO.getBookId());
         preparedStatement.setString(2, bookRentDTO.getCustomerId());
         preparedStatement.setInt(3,bookRentDTO.getQuantity());
         return preparedStatement.executeUpdate() > 0;
+
     }
 }
