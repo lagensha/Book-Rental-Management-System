bd85d1622d01759f8284522c46e27de43af4e7b1
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-06-11 10:53:59 +0530

    Add BookRentDetailsRepositoryImpl for managing book rental details insertion

---
+++ src/main/java/edu/icet/ecom/repository/impl/BookRentDetailsRepositoryImpl.java
@@ -0,0 +1,29 @@
+package edu.icet.ecom.repository.impl;
+
+import edu.icet.ecom.dto.BookRentDTO;
+import edu.icet.ecom.repository.BookRentDetailsRepository;
+import util.CrudUtil;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class BookRentDetailsRepositoryImpl implements BookRentDetailsRepository {
+    @Override
+    public boolean insertRentDetails(List<BookRentDTO> bookRentDTOList) throws SQLException {
+        for (BookRentDTO bookRentDTO: bookRentDTOList) {
+            boolean insert =insertRentDetails(bookRentDTO);
+            if (!insert) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public boolean insertRentDetails(BookRentDTO bookRentDTO) throws SQLException {
+        return CrudUtil.execute("INSERT INTO rentbooks VALUES (?,?,?,?)",
+                bookRentDTO.getBookId(),
+                bookRentDTO.getCustomerId(),
+                bookRentDTO.getQuantity()
+
+        );
+    }
+}
